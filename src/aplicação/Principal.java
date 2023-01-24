84bd5b5159eeb8b59cd55caec7d163e33423cc17
chicoalive <chicoalive@gmail.com>
2023-01-23 21:12:33 -0300

    Acrescentado comentários ao código

--- src/aplicação/Principal.java
+++ src/aplicação/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        // 1ª Parte obter dados do usuário
         System.out.println("Exercício sobre Matrizes");
         System.out.print("Tamanho das linhas e colunas: ");
         int linhas = teclado.nextInt();
@@ -20,19 +21,12 @@ public class Principal {
                 matriz[i][j] = teclado.nextInt();
             }
         }
-        /* System.out.print("Você digitou: ");
-        for (int i = 0; i < linhas; i++) {
-            for(int j=0; j<colunas; j++){
-                System.out.print(matriz[i][j]+" ");
-               
-            }
-             System.out.println("");
-        } */
+        // 2ª Parte mostra resultados ao usuário
         System.out.println("");
         System.out.print("Digite o numero que deseja saber a posição: ");
         int numero = teclado.nextInt();
-        // Quando for quantidades de linha posso trocar por "matriz.length"
-        // Quando for linhas uso matriz[i].lengt que me retonar o vetor daquela linha
+        // Quando for sobre linha horizontal da matriz posso trocar por o tamanho por "matriz.length"
+        // Quando for sobre linhas verticais da matriz posso trocar por "matriz[i].lengt" que vai me retonar os valores daquela linha
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 if (matriz[i][j] == numero) {
